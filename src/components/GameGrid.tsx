49919d8f2260060a039b6f687e3fe2d8846ae0cc
Haf <haf@protonmail.ch>
2022-10-30 22:02:43 +0100

    added birth factor and backing grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,18 +1,24 @@
 
+import { useState } from "react";
 import Cell from "./Cell";
 
 type GameGridProps = {
     height: number;
     width: number;
+    birthFactor: number;
 }
 
 const GameGrid = (props: GameGridProps) => {
+    const [ grid, setGrid ] = useState([...Array(props.height)].map((_) => 
+        [...Array(props.height)].map((_) => Math.random() < props.birthFactor ? true : false)
+    ))
+
     return (
         <div>
-            {[...Array(props.height)].map((_, y) => (
-                [...Array(props.width)].map((_, x) => (
+            { grid.map((_, y) => (
+                grid[y].map((_, x) => (
                     x < (props.width-1) 
-                    ? <Cell isAlive={true} aliveElement={<span>X</span>} deadElement={<span>O</span>} />
+                    ? <Cell isAlive={grid[y][x]} aliveElement={<span>X</span>} deadElement={<span>O</span>} />
                     : <br/>
                 ))
             ))}
@@ -23,6 +29,7 @@ const GameGrid = (props: GameGridProps) => {
 GameGrid.defaultProps = {
     height: 10,
     width: 10,
+    birthFactor: 0.2,
 }
 
 export default GameGrid

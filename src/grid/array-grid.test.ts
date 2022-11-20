873849eaf8a745cb04cadf28a36ff77b442e777f
Haf <haf@protonmail.ch>
2022-11-20 08:22:34 +0100

    actually tested live neighbours

--- src/grid/array-grid.test.ts
+++ src/grid/array-grid.test.ts
@@ -5,48 +5,63 @@ class FakeGrid extends Grid<FakeGrid> {
   constructor(
     readonly height: number,
     readonly width: number,
-    get: (x: number, y: number) => boolean = jest.fn()
   ) {
     super();
-    this.get = get;
   }
 
-  //get: (x: number, y: number) => boolean
+  get(x: number, y: number): boolean {
+    // Only the cell over (2,2) and the cell under it are alive, to test
+    // liveNeighbours
+    return (x === 2 && y === 3) || (x === 2 && y === 1)
+  }
+
   tick: (strategy: ConwayStrategy) => FakeGrid = jest.fn();
 }
 
-test("neighbours", () => {
-  // ARRANGE
-  const grid = new FakeGrid(5, 5);
-  
-  // ACT
-  const neighbours = grid.getNeighbours(2, 2);
-
-  // ASSERT
-  // sorting to ignore order
-  expect(neighbours.sort()).toEqual([
-    [1, 2],
-    [3, 2],
-    [2, 1],
-    [2, 3],
-  ].sort());
-});
-
-describe("contains", () => {
-  test("true", () => {
+describe("Grid", () => {
+  test("neighbours", () => {
     // ARRANGE
     const grid = new FakeGrid(5, 5);
 
-    // ACT & ASSERT
-    expect(grid.contains(2, 2)).toBe(true);
+    // ACT
+    const neighbours = grid.getNeighbours(2, 2);
+
+    // ASSERT
+    // sorting to ignore order
+    expect(neighbours.sort()).toEqual([
+      [1, 2],
+      [3, 2],
+      [2, 1],
+      [2, 3],
+    ].sort());
   });
 
-  test("false", () => {
+  describe("contains", () => {
+    test("true", () => {
+      // ARRANGE
+      const grid = new FakeGrid(5, 5);
+
+      // ACT & ASSERT
+      expect(grid.contains(2, 2)).toBe(true);
+    });
+
+    test("false", () => {
+      // ARRANGE
+      const grid = new FakeGrid(5, 5);
+
+      // ACT & ASSERT
+      expect(grid.contains(10, 10)).toBe(false);
+    })
+
+  });
+
+  test("liveNeighbours", () => {
     // ARRANGE
     const grid = new FakeGrid(5, 5);
 
     // ACT & ASSERT
-    expect(grid.contains(10, 10)).toBe(false);
+    expect(grid.liveNeighbours(2, 2)).toBe(2);
+
   })
 
 });

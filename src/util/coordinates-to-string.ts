02c624fc480474a6df7ff19632f8f2ba5071220c
Haf <haf@protonmail.ch>
2022-12-01 18:09:35 +0100

    removed monstruous coordinate string cache

--- src/util/coordinates-to-string.ts
+++ src/util/coordinates-to-string.ts
@@ -1,28 +1,7 @@
 import { Coordinates } from "../grid/grid";
 
-// Cache to prevent creating too many strings
-const INITIAL_COORDINATES_TO_STRING_CACHE_SIZE = 100;
-let coordinatesToStringCache = createCoordinatesToStringCache(INITIAL_COORDINATES_TO_STRING_CACHE_SIZE);
-
-function createCoordinatesToStringCache(size: number): string[][] {
-    return [... new Array(size)].map(() => new Array(size));
-}
-
 const coordinatesToString = ([x, y]: Coordinates): string => {
-    if (x < 0 || y < 0) {
-        return `(${x},${y})`;
-    } else if (x > coordinatesToStringCache.length - 1 || y > coordinatesToStringCache.length - 1) {
-        coordinatesToStringCache = createCoordinatesToStringCache(Math.max(x, y) + 1);
-    }
-
-    const cached = coordinatesToStringCache[y][x];
-    if (cached) {
-        return cached;
-    } else {
-        const value = `(${x},${y})`;
-        coordinatesToStringCache[y][x] = value;
-        return value;
-    }
+    return `(${x},${y})`
 };
 
 const COORDINATES_REGEX = /\((\d+),(\d+)\)/

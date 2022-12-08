ca88473ae75c22d479919aa687a60b0029b69d4b
Haf <haf@protonmail.ch>
2022-12-08 13:29:09 +0100

    indicator to restore settings but it's fake and it doesn't work

--- src/components/SlowIndicator.tsx
+++ src/components/SlowIndicator.tsx
@@ -9,21 +9,38 @@ const SlowIndicator: FC<{ tracker: PerformanceTracker }> = ({ tracker }) => {
         return null;
     }
 
-    return (<div className={classNames(
-        "fixed",
-        "bottom-5",
-        "left-5",
-        "bg-red-500",
-        "text-slate-100",
-        "rounded-lg",
-        "px-3",
-        "py-1",
-        "w-50",
-        "flex",
-        "opacity-90",
-    )}>
-        <FontAwesomeIcon icon={faExclamationCircle} className="w-5 mr-2" /> Ticking is slow
-    </div>)
+    return (
+        <div className={classNames(
+            "fixed",
+            "bottom-5",
+            "left-10",
+            "text-slate-100",
+            "w-50",
+        )}>
+            <div className={classNames(
+                "px-3",
+                "py-1",
+                "flex",
+                "opacity-90",
+                "bg-red-500",
+                "rounded-lg",
+                "text",
+                "mb-1",
+            )}>
+                <FontAwesomeIcon icon={faExclamationCircle} className="w-5 mr-2" /> Ticking is slow
+            </div>
+            <div className={classNames(
+                "px-2",
+                "py-1",
+                "opacity-90",
+                "bg-red-500",
+                "rounded-lg",
+                "text-xs",
+            )}>
+                {/*<p className="mb-1">Some features have been disabled</p> */}
+                <p className="font-bold">Click here to restore settings</p>
+            </div>
+        </div>)
 };
 
 export default SlowIndicator;

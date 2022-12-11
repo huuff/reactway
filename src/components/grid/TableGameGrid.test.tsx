df5d9f9aaace0e1feb22b7fa9f1f12bada93c11e
Haf <haf@protonmail.ch>
2022-12-11 16:27:06 +0100

    tested toggling for TableGameGrid

--- src/components/grid/TableGameGrid.test.tsx
+++ src/components/grid/TableGameGrid.test.tsx
@@ -1,5 +1,5 @@
 import TableGameGrid from "./TableGameGrid";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { SetGrid } from "../../grid/set-grid";
 import { coordinatesToString } from "../../util/coordinates-to-string";
 import { Coordinates } from "../../grid/grid";
@@ -14,7 +14,6 @@ jest.mock("usehooks-ts", () => ({
     }))
   }));
 
-  // TODO: Test toggling?
 const liveCells: Coordinates[] = [tuple(2, 3), tuple(2, 1), tuple(3, 2), tuple(3,3), tuple(4, 2)];
 describe("TableGameGrid", () => {
     test("live cells have the alive color", () => {
@@ -29,6 +28,19 @@ describe("TableGameGrid", () => {
         render(<TableGameGrid grid={new SetGrid(liveCells)} toggleCell={jest.fn()} cellSize={3}/>);
 
         expect(screen.getByTestId(coordinatesToString(tuple(2, 2)))).toHaveClass(`bg-${theme.light.cell.dead.className}`);
+    });
+
+    test("clicking on a cell toggles it", () => {
+        // ARRANGE
+        const toggle = jest.fn();
+        render(<TableGameGrid grid={new SetGrid(liveCells)} toggleCell={toggle} cellSize={3}/>);
+        const targetCell = tuple(2, 2);
+
+        // ACT
+        fireEvent.click(screen.getByTestId(coordinatesToString(targetCell)), { bubbles: true });
+
+        // ASSERT
+        expect(toggle).toHaveBeenCalledWith(targetCell);
     });
 
     test("snapshot", () => {

764db1a6d6e6ba16b3bd51d19b82a2f0a3127204
Haf <haf@protonmail.ch>
2022-12-11 12:43:19 +0100

    fixed the canvas test

--- src/components/grid/CanvasGameGrid.test.tsx
+++ src/components/grid/CanvasGameGrid.test.tsx
@@ -4,10 +4,24 @@ import { Coordinates } from "../../grid/grid";
 import { SetGrid } from "../../grid/set-grid";
 import tuple from "immutable-tuple";
 
+// https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+
 const liveCells: Coordinates[] = [tuple(2, 3), tuple(2, 1), tuple(3, 2), tuple(3,3), tuple(4, 2)];
 describe("CanvasGameGrid", () => {
-    // Won't work (suddenly I need to mock refs? Maybe I should use enzyme)
-    test.skip("snapshot", () => {
+    test("snapshot", () => {
         expect(renderer.create(
             <CanvasGameGrid 
                 grid={new SetGrid(liveCells)}

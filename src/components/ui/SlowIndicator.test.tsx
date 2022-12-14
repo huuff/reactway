e466497bfd0ac666cb21e642c5aa880873003804
Haf <haf@protonmail.ch>
2022-12-14 19:23:30 +0100

    reactor and another test for the slow indicator

--- src/components/ui/SlowIndicator.test.tsx
+++ src/components/ui/SlowIndicator.test.tsx
@@ -2,19 +2,19 @@ import { render, screen } from "@testing-library/react";
 import { PerformanceTracker, PerformanceTrackerContext } from "../../hooks/use-performance-tracker";
 import SlowIndicator from "./SlowIndicator";
 
+const fakePerformanceTracker: PerformanceTracker = {
+    isSlow: false,
+    averageOverhead: 0,
+    recordSample: jest.fn(),
+    disabledFeatures: [],
+    isDisabled: jest.fn(),
+    updateBatches: jest.fn(),
+    reset: jest.fn(),
+};
+
 describe("SlowIndicator", () => {
 
     test("it doesn't show when there are no performance issues", () => {
-        const fakePerformanceTracker: PerformanceTracker = {
-            isSlow: false,
-            averageOverhead: 0,
-            recordSample: jest.fn(),
-            disabledFeatures: [],
-            isDisabled: jest.fn(),
-            updateBatches: jest.fn(),
-            reset: jest.fn(),
-        };
-
         render(
             <PerformanceTrackerContext.Provider value={fakePerformanceTracker}>
                 <SlowIndicator resetSettings={jest.fn()} />
@@ -22,5 +22,17 @@ describe("SlowIndicator", () => {
         );
 
         expect(screen.queryByText("Ticking is slow")).not.toBeInTheDocument();
+    });
+
+    test("it shows when ticking is slow", () => {
+        const slowTracker = { ...fakePerformanceTracker, isSlow: true };
+
+        render(
+            <PerformanceTrackerContext.Provider value={slowTracker}>
+                <SlowIndicator resetSettings={jest.fn()} />
+            </PerformanceTrackerContext.Provider>
+        );
+
+        expect(screen.queryByText("Ticking is slow")).toBeInTheDocument();
     });
 });

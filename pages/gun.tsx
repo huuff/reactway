e5f6b27b79f3f45f8ce9517d46c4f986194d60d2
Haf <haf@protonmail.ch>
2022-12-15 19:45:05 +0100

    speed range for the gun

--- pages/gun.tsx
+++ pages/gun.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import { useMemo } from "react";
+import { ChangeEvent, useCallback, useMemo, useState } from "react";
 import { useDarkMode, useInterval } from "usehooks-ts";
 import CanvasGameGrid from "../src/components/grid/CanvasGameGrid";
 import DarkModeSelector from "../src/components/settings/DarkModeSelector";
@@ -7,6 +7,7 @@ import SlowIndicator from "../src/components/ui/SlowIndicator";
 import { useGrid } from "../src/game/use-grid";
 import { gridFromAscii } from "../src/util/create-grid-from-ascii";
 import { getTheme } from "../src/util/get-theme";
+import clamp from "lodash/clamp";
 
 const Gun = () => {
     const { isDarkMode } = useDarkMode();
@@ -34,9 +35,25 @@ const Gun = () => {
 
     const { grid, toggleCell, tick } = useGrid(initialGrid);
 
+    const [ intervalMultiplier, setIntervalMultiplier ] = useState(1);
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        console.log("Handling change");
+        setIntervalMultiplier(clamp(+e.target.value, -10, 10));
+    };
+
+    const actualMultiplier = useMemo(() => {
+        if (intervalMultiplier < 0) {
+            return -(intervalMultiplier);
+        } else if (intervalMultiplier > 0) {
+            return 1/intervalMultiplier;
+        } else {
+            return 1;
+        }
+    }, [intervalMultiplier]);
+
     useInterval(() => {
         tick();
-    }, 1000);
+    }, actualMultiplier && 1000 * actualMultiplier);
 
     return (
         <div className={`min-h-screen bg-${theme.windowBackground.className}`}>
@@ -52,6 +69,10 @@ const Gun = () => {
         >Gosper glider gun</header>
         <main>
             <CanvasGameGrid grid={grid} toggleCell={toggleCell} cellSize={2}/>
+            <div className="text-center mt-5 flex flex-col w-40 mx-auto">
+                <p className={`text-${theme.text.className}`}>Speed</p>
+                <input type="range" min={-10} step={1} max={10} value={intervalMultiplier} onChange={handleChange}/>
+            </div>
         </main>
     </div>
     );

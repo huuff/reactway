21b477e6fccb6e58d99f588a8b43137bea26cb4c
Haf <haf@protonmail.ch>
2022-12-16 12:18:10 +0100

    starting out with the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import classNames from "classnames";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import Header from "../src/components/ui/Header";
@@ -23,12 +25,49 @@ const Index = () => {
   }, [router]);
   */
 
+  // TODO: Make the link take to the game
   return (
     <div className={`h-screen bg-${theme.windowBackground.className}`}>
-      <Header text="Reactway"/>
+      <Header text="Reactway" />
       <SubHeader>
         Where dreams come true.
       </SubHeader>
+      <main className={classNames(
+        "mt-10",
+        `text-${theme.text.className}`,
+        "h-96",
+        "w-[42rem]",
+        `bg-${theme.panel.className}`,
+        "mx-auto",
+        "shadow-lg",
+        "rounded-md",
+        "p-5",
+        "text-center",
+        "flex",
+        "flex-col",
+        "justify-between",
+      )}>
+        <div>
+          <p className="my-1">
+            Before continuing, there are some things you <span className="italic">don&apos;t</span> need to know.
+          </p>
+          <p className="my-2">
+            Reactway is a react-based implementation of Conway&apos;s Game of Life
+          </p>
+          <p className="my-3">
+            Game of Life is a hyperrealistic simulation of a living community where individuals live, communicate, reproduce and die.
+          </p>
+          <p className="my-4">
+            It&apos;s said to be so good that most people can discern it from reality after a few hours. So proceed with caution
+          </p>
+          <p className="my-5">
+            Keep reading to know more about its rules and nature.
+          </p>
+        </div>
+        <div>
+          Or just <Link href="#" className="underline">start playing right now</Link>
+        </div>
+      </main>
     </div>
   );
 };
